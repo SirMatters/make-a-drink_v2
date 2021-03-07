b46c3e02adaee6a5d97602198b3d3e5fd9a72b9f
SirMatters <perov.krll@gmail.com>
2021-03-07 23:39:14 +0300

    Finalize starsrating component

--- frontend/components/StarRating.js
+++ frontend/components/StarRating.js
@@ -1,28 +1,73 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { FaStar } from 'react-icons/fa'
 
 const StarRatingStyles = styled.div`
-  list-style: none;
+  label {
+    list-style: none;
+    
+    input {
+      visibility: hidden;
+      margin: 0;
+      width: 0;
+      height: 0;
+    }  
+  }
 `
 
-export default function StarRating({ userVote }) {
+export default function StarRating({ 
+  maxVal,
+  currentRating,
+  disabled,
+  votesNum,
+  onChange,
+  totalRating
+}) {
 
-  const [clicked, setClicked] = useState();
-  const [hovered, setHovered] = useState();
+  const [rating, setRating] = useState(null);
+  const [hover, setHover] = useState(null);
 
-  const handleMouseEnter = () => {
-    
-  }
+  useEffect(() => {
+    setRating(Math.floor(currentRating))
+  }, [currentRating])
+
+  const handleClick = (ratingVal) => {
+    if (!disabled) {
+      setRating(ratingVal);
+      onChange(ratingVal);
+    }
+  };
+
+  const handleHover = (ratingVal) => {
+    if (!disabled) {
+      setHover(ratingVal);
+    }
+  };
 
   return (
     <StarRatingStyles>
-      <ul>
-        <li><input type='radio' data-pos="1" onMouseEnter={}/></li>
-        <li><input type='radio' data-pos="2"/></li>
-        <li><input type='radio' data-pos="3"/></li>
-        <li><input type='radio' data-pos="4"/></li>
-        <li><input type='radio' data-pos="5"/></li>
-      </ul>
+      {
+        [...Array(maxVal ? maxVal : 5)].map((s,i) => {
+          const ratingVal = i + 1;
+          
+          return (
+            <label className='rating-star' key={`star-${i}`}>
+              <input type='radio' name='rating' value={ratingVal}/>
+              <FaStar
+                className='star'
+                size={20}
+                color={ratingVal <= (hover || rating) ? '#ffc107' : '#e4e5e9'}
+                onClick={() => handleClick(ratingVal)}
+                onMouseEnter={() => handleHover(ratingVal)}
+                onMouseLeave={() => handleHover(null)}
+                />
+          </label>
+          );
+        })
+      }
+      <span>
+        {totalRating}({votesNum})
+      </span>
     </StarRatingStyles>
   )
 }
