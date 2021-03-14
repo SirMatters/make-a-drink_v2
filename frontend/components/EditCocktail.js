24a76c108cc17e42918afe09cfcd6a83907972f5
SirMatters <perov.krll@gmail.com>
2021-03-14 13:22:54 +0300

    add simple styling to EditCocktail

--- frontend/components/EditCocktail.js
+++ frontend/components/EditCocktail.js
@@ -1,5 +1,6 @@
 import { useMutation, useQuery } from '@apollo/client'
 import { gql } from 'graphql-tag'
+import styled from 'styled-components'
 import useForm from '../lib/useForm'
 import {SINGLE_COCKTAIL_QUERY} from './CocktailView'
 
@@ -21,6 +22,102 @@ const UPDATE_COCKTAIL_MUTATION = gql`
   }
 `
 
+const EditCocktailStyles = styled.form`
+  box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.05);
+  background: rgba(0, 0, 0, 0.02);
+  border: 5px solid white;
+  padding: 20px;
+  font-size: 1.5rem;
+  line-height: 1.5;
+  font-weight: 600;
+  label {
+    display: block;
+    margin-bottom: 1rem;
+  }
+  input,
+  textarea,
+  select {
+    width: 100%;
+    padding: 0.5rem;
+    font-size: 1rem;
+    border: 1px solid black;
+    &:focus {
+      outline: 0;
+      border-color: var(--red);
+    }
+  }
+
+  button,
+  input[type='submit'] {
+    width: auto;
+    background: red;
+    color: white;
+    border: 0;
+    font-size: 2rem;
+    font-weight: 600;
+    padding: 0.5rem 1.2rem;
+  }
+
+    width: 100%;
+    max-width: 100%;
+    min-height: 40rem;
+
+    fieldset {
+      height: 35rem;
+      border: none;
+
+      &[disabled] {
+        opacity: 0.5;
+      }
+
+      display: grid;
+      grid-template-columns: 1.5fr 2fr;
+      grid-template-rows: 4rem 2rem auto 1fr;
+      grid-template-areas:
+      "img name"
+      "img description"
+      "img steps";
+      grid-column-gap: 2rem;
+    }
+
+  .cocktail-image {
+    grid-area: img;
+    width: 100%;
+    height: 100%;
+
+    background-image: ${(props) => `url(${props.img})`};
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+  }
+
+  .cocktail-name {
+    grid-area: name;
+    align-self: start;
+    margin: 0;
+  }
+
+  .cocktail-description {
+    margin-top: 1rem;
+    grid-area: description;
+
+    textarea {
+      display: block;
+    }
+  }
+
+  .cocktail-steps {
+    margin-top: 1rem;
+    grid-area: steps;
+
+    ol {
+      margin: 0;
+      padding-top: 0;
+      padding-left: 1.6rem;
+    }
+  }
+`
+
 export default function EditCocktail({id}) {
   const {data, error, loading} = useQuery(SINGLE_COCKTAIL_QUERY, {variables: { id }})
   const {inputs, handleChange} = useForm(data?.Cocktail);
@@ -33,24 +130,25 @@ export default function EditCocktail({id}) {
   if (loading) return <p>Loading...</p>
 
   return (
-    <div>
-      <form
-        onSubmit={ async (e) => {
-          e.preventDefault();
-          await updateCocktail({
-            variables: {
-              id,
-              name: inputs.name,
-              description: inputs.description,
-              // TODO: add steps update
-            }
-          })
-        }}
-      > 
+    <EditCocktailStyles
+      className="cocktail-section"
+      onSubmit={ async (e) => {
+        e.preventDefault();
+        await updateCocktail({
+          variables: {
+            id,
+            name: inputs.name,
+            description: inputs.description,
+            // TODO: add steps update
+          }
+        })
+      }}
+    >
         {/* TODO: add image upload */}
         {(error || updateError) && <p>Error !!!</p>}
         <fieldset disabled={updateLoading || loading}>
-          <label htmlFor="name">
+          <div className="cocktail-image">IMAGE upload</div>
+          <label htmlFor="name" className="cocktail-name">
             Name
             <input
               id="name"
@@ -59,7 +157,7 @@ export default function EditCocktail({id}) {
               onChange={handleChange}
             />
           </label>
-          <label htmlFor="description">
+          <label htmlFor="description" className="cocktail-description">
             Description
             <textarea
               id="description"
@@ -68,9 +166,11 @@ export default function EditCocktail({id}) {
               onChange={handleChange}
             />
           </label>
+          {/* <label htmlFor="cocktail-steps">
+            <input type="text"/>
+          </label> */}
         </fieldset>
         <button type="submit">Save cocktail</button>
-      </form>
-    </div>
+    </EditCocktailStyles>
   )
 }
--- frontend/pages/cocktails/[id]/edit.js
+++ frontend/pages/cocktails/[id]/edit.js
@@ -2,8 +2,6 @@ import EditCocktail from "../../../components/EditCocktail";
 
 export default function editCocktail({query: {id}}) {
   return(
-    <div>
       <EditCocktail id={id}/>
-    </div>
   )
 }
